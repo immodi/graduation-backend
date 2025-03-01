db5471e2cf60fec323e48271edc6ce66c4dee05b
immodi <ay727468a@gmail.com>
2025-03-01 19:45:53 +0200

    no https redirect

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
